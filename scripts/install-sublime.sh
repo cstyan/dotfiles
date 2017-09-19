2f9ae35d0184c760733ecb1e51ce429c27a6aa27
Callum Styan <callumstyan@gmail.com>
2017-09-19 13:46:38 -0700

    update to the latest build of sublime

--- scripts/install-sublime.sh
+++ scripts/install-sublime.sh
@@ -1,4 +1,4 @@
-SUBL_VERSION=3126
+SUBL_VERSION=3143
 curl -O https://download.sublimetext.com/sublime-text_build-"$SUBL_VERSION"_amd64.deb
 dpkg -i sublime-text_build-"$SUBL_VERSION"_amd64.deb
 rm sublime-text_build-"$SUBL_VERSION"_amd64.deb
