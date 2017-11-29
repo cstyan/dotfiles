9087c1b1a1a22a430cfdd08ad591b26037552dac
Callum Styan <callumstyan@gmail.com>
2017-11-28 22:06:22 -0800

    install newest version of k8s and minikube

--- scripts/install-k8s.sh
+++ scripts/install-k8s.sh
@@ -1,4 +1,4 @@
-MINIKUBE_VERSION=v0.20.0 
+MINIKUBE_VERSION=v0.23.0
 
 echo "**** Install kubectl ****"
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
