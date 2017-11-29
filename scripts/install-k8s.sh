793f747c297ea3e933c5d086c3d8b46b681daf51
Callum Styan <callumstyan@gmail.com>
2017-11-28 23:28:00 -0800

    install libvirt and kvm for minikube since virtualbox is bad news

--- scripts/install-k8s.sh
+++ scripts/install-k8s.sh
@@ -8,3 +8,6 @@ echo "**** Install minikube ****"
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/"$MINIKUBE_VERSION"/minikube-linux-amd64 
 chmod +x minikube 
 mv minikube /usr/local/bin/
+echo "**** Install kvm & libvirt ****"
+sudo apt install libvirt-bin qemu-kvm
+sudo usermod -a -G libvirtd $(whoami)
