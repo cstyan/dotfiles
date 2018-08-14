151f58d5ae4fd0b9fc867f78a306da137654d9cc
Callum Stytan <callumstyan@gmail.com>
2018-08-14 10:50:49 -0700

    use alacritty instead of urxvt

--- arch/provision.sh
+++ arch/provision.sh
@@ -37,6 +37,7 @@ mkdir -p ~/.config/dunst
 mkdir -p ~/.config/ranger
 ln -sf $DOTFILES_REPO/dotfiles/.zshrc ~/.zshrc
 ln -sf $DOTFILES_REPO/dotfiles/.Xresources ~/.Xresources
+ln -sf $DOTFILES_REPO/dotfiles/alacritty.yml ~/.config/alacritty/alacritty.yml
 ln -sf $DOTFILES_REPO/dotfiles/i3-config ~/.config/i3/config
 ln -sf $DOTFILES_REPO/dotfiles/i3status-config ~/.config/i3status/config
 ln -sf $DOTFILES_REPO/dotfiles/redshift.conf ~/.config/redshift.conf
