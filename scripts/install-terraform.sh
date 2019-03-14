278eff4e6ea276fe7b2ff3da2c74e175e2e2de64
Callum Styan <callumstyan@gmail.com>
2019-03-14 15:44:23 -0700

    add script for installing terraform
    Signed-off-by: Callum Styan <callumstyan@gmail.com>

---
+++ scripts/install-terraform.sh
@@ -0,0 +1,9 @@
+TF_VERSION=0.11.13
+TF_ZIP=terraform_"$TF_VERSION"_linux_amd64.zip
+
+# https://releases.hashicorp.com/terraform/0.11.13/terraform_0.11.13_linux_amd64.zip
+
+curl -LO https://releases.hashicorp.com/terraform/"$TF_VERSION"/"$TF_ZIP"
+unzip $TF_ZIP
+mv terraform /usr/local/bin/.
+rm $TF_ZIP
